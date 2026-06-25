1b52323a358ff5df72172f839f40b5a333b79151
v-s-android <vin.anddev@gmail.com>
2026-06-24 20:37:49 -0400

    Use OpenAI's Whisper model

--- simple_speech2text.py
+++ simple_speech2text.py
@@ -1,20 +1,20 @@
-import requests
+from transformers import pipeline
 
-# URL of the audio file to be downloaded
-url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-GPXX04C6EN/Testing%20speech%20to%20text.mp3"
+# Initialize the speech-to-text pipeline from Hugging Face Transformers
+  #- This uses the "openai/whisper-tiny.en" model for automatic speech recognition (ASR)
+  #- The `chunk_length_s` parameter specifies the chunk length in seconds for processing
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model = "openai/whisper-tiny.en",
+    chunk_length_s = 30
+)
 
-# Send a GET request to the URL to download the file
-response = requests.get(url)
+## Define the path to the audio file that needs to be transcribed
+sample_audio = "downloaded_audio.mp3"
 
-# Define the local file path where the audio file will be saved
-audio_file_path = "downloaded_audio.mp3"
+# Perform speech recognition on the audio file
+ #- The `batch_size=8` parameter indicates how many chunks are processed at a time
+ #- The result is stored in `prediction` with the key "text" containing the transcribed text
+prediction = pipe(sample_audio, batch_size=8)["text"]
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # If successful, write the content to the specified local file path
-    with open(audio_file_path, 'wb') as file:
-        file.write(response.content)
-        print("File downloaded successfully")
-else:
-    # If the request failed, print an error message
-    print("Failed to download")
+print(prediction)
